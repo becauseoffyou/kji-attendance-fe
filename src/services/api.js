043a1724abb8193378700ed697d70e2641a44ffc
becauseoffyou <irhandy96@gmail.com>
2026-07-28 16:23:39 +0700

    fixing token

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,20 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://kji-attendance-be-production.up.railway.app/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
+
+api.interceptors.request.use(
+    (config) => {
+
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default api;
