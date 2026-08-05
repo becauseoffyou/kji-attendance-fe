abb2fdecbe15598eb7f80110f18850c1e6a8b3ea
becauseoffyou <irhandy96@gmail.com>
2026-08-05 17:22:13 +0700

    absensi s3

--- src/pages/attendance/AdminAttendance.jsx
+++ src/pages/attendance/AdminAttendance.jsx
@@ -14,9 +14,20 @@ import {
 
 import DownloadIcon from "@mui/icons-material/Download";
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import dayjs from "dayjs";
+
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 
 export default function AdminAttendance() {
-
+    const [filters, setFilters] = useState({
+        startDate: "",
+        endDate: "",
+        search: "",
+        department: "",
+        status: ""
+    });
     return (
 
         <>
@@ -82,10 +93,35 @@ export default function AdminAttendance() {
 
                     <Grid container spacing={2}>
 
-                        <Grid size={{ xs: 12, md: 3 }}>
+                        <Grid size={{ xs: 12, md: 2.4 }}>
                             <TextField
                                 fullWidth
                                 type="date"
+                                label="Dari"
+                                InputLabelProps={{ shrink: true }}
+                                value={filters.startDate}
+                                onChange={(e) =>
+                                    setFilters({
+                                        ...filters,
+                                        startDate: e.target.value
+                                    })
+                                }
+                            />
+                        </Grid>
+
+                        <Grid size={{ xs: 12, md: 2.4 }}>
+                            <TextField
+                                fullWidth
+                                type="date"
+                                label="Sampai"
+                                InputLabelProps={{ shrink: true }}
+                                value={filters.endDate}
+                                onChange={(e) =>
+                                    setFilters({
+                                        ...filters,
+                                        endDate: e.target.value
+                                    })
+                                }
                             />
                         </Grid>
 
