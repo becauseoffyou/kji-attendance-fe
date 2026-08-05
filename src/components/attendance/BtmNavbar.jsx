8e48ce29d5ef1264b9f1e7d88fab19c09016160b
becauseoffyou <irhandy96@gmail.com>
2026-08-05 11:53:26 +0700

    fixing async v1

--- src/components/attendance/BtmNavbar.jsx
+++ src/components/attendance/BtmNavbar.jsx
@@ -18,8 +18,11 @@ export default function BottomNav() {
   const role = user?.role?.toUpperCase() || "";
   const [badge, setBadge] = useState(0);
   const showBadge =
-    (user.role === "SUPERVISOR" && item.value === "/employee/approval") ||
-    (user.role === "EMPLOYEE" && item.value === "/employee/leave");
+    (user?.role === "SUPERVISOR" &&
+      item.value === "/employee/approval") ||
+
+    (user?.role === "EMPLOYEE" &&
+      item.value === "/employee/leave");
 
 
   const canApproval = [
