803b41da49a77ae85c91cd515aaaee0a5f659541
becauseoffyou <irhandy96@gmail.com>
2026-08-05 17:34:22 +0700

    absensi s3-4

--- src/pages/attendance/AdminAttendance.jsx
+++ src/pages/attendance/AdminAttendance.jsx
@@ -158,7 +158,7 @@ export default function AdminAttendance() {
 
                             {/* Search */}
 
-                            <Grid size={{ xs: 12, md: 3 }}>
+                            <Grid size={{ xs: 12, md: 2 }}>
 
                                 <TextField
                                     fullWidth
